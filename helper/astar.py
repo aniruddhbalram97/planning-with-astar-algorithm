e0410e263b9fef2430ff4d3d72433c9cc822c3e2
Aniruddh Balram <abalram1@umd.edu>
2024-02-14 18:35:42 -0500

    Add AStar member functions
    * run: Implements AStar planning algorithm

--- helper/astar.py
+++ helper/astar.py
@@ -3,10 +3,11 @@ import numpy as np
 from queue import PriorityQueue
 
 class AStar:
-    def __init__(self, initial_node, goal_node, obstacle_map):
+    def __init__(self, initial_node, goal_node, obstacle_map, step_size):
         self.initial_node = initial_node
         self.goal_node = goal_node
         self.obstacle_map = obstacle_map
+        self.step_size = step_size
         
         self.open_list = PriorityQueue()
         self.close_list = np.zeros((500, 1200, 12), np.uint8)
@@ -20,11 +21,11 @@ class AStar:
         self.cost_of_node[self.initial_node] = 0
         return
     
-    def move_neg_sixty(self, current_node, step_size):  
+    def move_neg_sixty(self, current_node):  
         next_node = []
         theta = -60
-        next_x = int(round((current_node[0] + step_size * (math.cos((current_node[2] + theta) * (math.pi/180))))))
-        next_y = int(round((current_node[1] + step_size * (math.sin((current_node[2] + theta) * (math.pi/180))))))
+        next_x = int(round((current_node[0] + self.step_size * (math.cos((current_node[2] + theta) * (math.pi/180))))))
+        next_y = int(round((current_node[1] + self.step_size * (math.sin((current_node[2] + theta) * (math.pi/180))))))
         next_orientation = theta + current_node[2]
 
         # For negative index in the visited matrix
@@ -35,13 +36,13 @@ class AStar:
         next_node.append(next_x)
         next_node.append(next_y)
         next_node.append(next_orientation)
-        return step_size, tuple(next_node)
+        return self.step_size, tuple(next_node)
     
-    def move_neg_thirty(self, current_node, step_size):  
+    def move_neg_thirty(self, current_node):  
         next_node = []
         theta = -30
-        next_x = int(round((current_node[0] + step_size * (math.cos((current_node[2] + theta) * (math.pi/180))))))
-        next_y = int(round((current_node[1] + step_size * (math.sin((current_node[2] + theta) * (math.pi/180))))))
+        next_x = int(round((current_node[0] + self.step_size * (math.cos((current_node[2] + theta) * (math.pi/180))))))
+        next_y = int(round((current_node[1] + self.step_size * (math.sin((current_node[2] + theta) * (math.pi/180))))))
         next_orientation = theta + current_node[2]
         
         # For negative index in the visited matrix
@@ -52,13 +53,13 @@ class AStar:
         next_node.append(next_x)
         next_node.append(next_y)
         next_node.append(next_orientation)
-        return step_size, tuple(next_node)
+        return self.step_size, tuple(next_node)
     
-    def move_zero(self, current_node, step_size):  
+    def move_zero(self, current_node):  
         next_node = []
         theta = 0
-        next_x = int(round((current_node[0] + step_size * (math.cos((current_node[2] + theta) * (math.pi/180))))))
-        next_y = int(round((current_node[1] + step_size * (math.sin((current_node[2] + theta) * (math.pi/180))))))
+        next_x = int(round((current_node[0] + self.step_size * (math.cos((current_node[2] + theta) * (math.pi/180))))))
+        next_y = int(round((current_node[1] + self.step_size * (math.sin((current_node[2] + theta) * (math.pi/180))))))
         next_orientation = theta + current_node[2]
         
         # For negative index in the visited matrix
@@ -69,13 +70,13 @@ class AStar:
         next_node.append(next_x)
         next_node.append(next_y)
         next_node.append(next_orientation)
-        return step_size, tuple(next_node)
+        return self.step_size, tuple(next_node)
     
-    def move_pos_thirty(self, current_node, step_size):  
+    def move_pos_thirty(self, current_node):  
         next_node = []
         theta = 30
-        next_x = int(round((current_node[0] + step_size * (math.cos((current_node[2] + theta) * (math.pi/180))))))
-        next_y = int(round((current_node[1] + step_size * (math.sin((current_node[2] + theta) * (math.pi/180))))))
+        next_x = int(round((current_node[0] + self.step_size * (math.cos((current_node[2] + theta) * (math.pi/180))))))
+        next_y = int(round((current_node[1] + self.step_size * (math.sin((current_node[2] + theta) * (math.pi/180))))))
         next_orientation = theta + current_node[2]
         
         # For negative index in the visited matrix
@@ -86,13 +87,13 @@ class AStar:
         next_node.append(next_x)
         next_node.append(next_y)
         next_node.append(next_orientation)
-        return step_size, tuple(next_node)
+        return self.step_size, tuple(next_node)
     
-    def move_pos_sixty(self, current_node, step_size):  
+    def move_pos_sixty(self, current_node):  
         next_node = []
         theta = 60
-        next_x = int(round((current_node[0] + step_size * (math.cos((current_node[2] + theta) * (math.pi/180))))))
-        next_y = int(round((current_node[1] + step_size * (math.sin((current_node[2] + theta) * (math.pi/180))))))
+        next_x = int(round((current_node[0] + self.step_size * (math.cos((current_node[2] + theta) * (math.pi/180))))))
+        next_y = int(round((current_node[1] + self.step_size * (math.sin((current_node[2] + theta) * (math.pi/180))))))
         next_orientation = theta + current_node[2]
         
         # For negative index in the visited matrix
@@ -103,15 +104,15 @@ class AStar:
         next_node.append(next_x)
         next_node.append(next_y)
         next_node.append(next_orientation)
-        return step_size, tuple(next_node)
+        return self.step_size, tuple(next_node)
     
-    def valid_next_steps(self, current_node, step_size):
+    def valid_next_steps(self, current_node):
         valid_neighbors = []
-        neg_sixty = self.move_neg_sixty(current_node, step_size)
-        neg_thirty = self.move_neg_thirty(current_node, step_size)
-        zero = self.move_zero(current_node, step_size)
-        pos_thirty = self.move_pos_thirty(current_node, step_size)
-        pos_sixty = self.move_pos_sixty(current_node, step_size)
+        neg_sixty = self.move_neg_sixty(current_node)
+        neg_thirty = self.move_neg_thirty(current_node)
+        zero = self.move_zero(current_node)
+        pos_thirty = self.move_pos_thirty(current_node)
+        pos_sixty = self.move_pos_sixty(current_node)
         
         if((neg_sixty[1][0] - 5)< 0 or (neg_sixty[1][0] + 5) > 599 
         or (neg_sixty[1][1] - 5) < 0 or (neg_sixty[1][1] + 5) > 249
@@ -228,3 +229,31 @@ class AStar:
         else:
             valid_neighbors.append(pos_sixty) 
         return valid_neighbors
+    
+    def run(self):
+        while self.open_list:
+            current_node = self.open_list.get()[1]
+            self.close_list[int(round(current_node[1] * 2)),int(round(current_node[0] * 2)), int(round(current_node[2]/30))] = 2
+            self.explored_list.append(current_node)
+            if(math.sqrt((current_node[0] - self.goal_node[0])**2 + (current_node[1] - self.goal_node[1])**2) < 0.5 and 
+            current_node[2] == self.goal_node[2]):
+                print("\n.. GOAL REACHED ..\n")
+                break
+            else:
+                valid_neighbors = self.valid_next_steps(current_node)
+                for next_node in valid_neighbors:
+                    closed = self.close_list[int(round(next_node[1][1] * 2)),int(round(next_node[1][0] * 2)), \
+                        int(round(next_node[1][2]/30))]
+                    # if the node is already present, ignore it
+                    if(closed == 2):
+                        print("visited: ", next_node)
+                        continue    
+                    else:
+                        cost_to_next_node = next_node[0]
+                        total_cost_to_node = self.cost_of_node[current_node] + cost_to_next_node
+                        if(next_node[1] not in self.cost_of_node or total_cost_to_node < self.cost_of_node[next_node[1]]):
+                            self.cost_of_node[next_node[1]] = total_cost_to_node
+                            euc_dist = math.sqrt((next_node[1][0] - self.goal_node[0])**2 + (next_node[1][1] - self.goal_node[1])**2)
+                            cost_with_heuristic = total_cost_to_node + euc_dist       
+                            self.open_list.put((cost_with_heuristic, next_node[1]))
+                            self.parent_node[next_node[1]] = current_node 
