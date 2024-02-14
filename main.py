e484d93f62fc911d3d1c0d2c75341154d78b9c15
Aniruddh Balram <abalram1@umd.edu>
2024-02-14 18:54:16 -0500

    Add main script
    * Create map instance
    
    * Create user input instance
    
    * Create Astar instance and run the algorithm

---
+++ main.py
@@ -0,0 +1,19 @@
+from helper.map import Map
+from helper.user_input import UserInput
+from helper.astar import AStar
+
+if __name__=="__main__":
+    map = Map(600, 250, 5, 75)
+    map.generate_map()
+    map.display_map()
+    obstacle_map = map.get_map()
+    
+    input = UserInput(obstacle_map)
+    initial_node, goal_node, step_size = input.take_input()
+    
+    astar = AStar(initial_node, goal_node, obstacle_map, step_size)
+    astar.run()
+    found_path = astar.generate_path()
+    explored_list = astar.get_explored_list()
+    
+        
